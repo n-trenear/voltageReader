379c023010d257d1ed4262d00674503331368f1e
n-trenear <44279370+n-trenear@users.noreply.github.com>
2019-03-06 15:39:39 +1000

    update

--- voltageReader.py
+++ voltageReader.py
@@ -1,11 +1,10 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-#set the alternate mode for pin 4 (GPCLK0)
-GPIO.setup(7,GPIO.ALT0)
+GPIO.setup(4,GPIO.ALT0)
 
 while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
+    if GPIO.input(15) == GPIO.HIGH:
         print("Button was pushed!")

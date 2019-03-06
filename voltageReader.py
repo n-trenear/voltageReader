01061d7a834a459bc81e232ed2770b3cc2e6cb4a
n-trenear <44279370+n-trenear@users.noreply.github.com>
2019-03-06 15:29:06 +1000

    update

--- voltageReader.py
+++ voltageReader.py
@@ -3,6 +3,9 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+#set the alternate mode for pin 4 (GPCLK0)
+GPIO.setup(7,GPIO.ALT0)
+
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")

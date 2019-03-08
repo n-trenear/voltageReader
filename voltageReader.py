920c8d779e700c7b83cce3b374108538e4dcddf4
n-trenear <44279370+n-trenear@users.noreply.github.com>
2019-03-08 10:05:07 +1000

    Update

--- voltageReader.py
+++ voltageReader.py
@@ -1,12 +1,22 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def button_callback(channel):
-    print("Button was pushed!")
+def input_callback1(channel):
+    print("1")
+    
+def input_callback0(channel):
+    print("0")
     
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(10,GPIO.RISING,callback=input_callback1) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(10,GPIO.RISING,callback=input_callback0) # Setup event on pin 10 rising edge
+
+## Setup the GPIO for PWM on pin12 {GPIO18}
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+p = GPIO.PWM(12, 20000000)
+p.start
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up

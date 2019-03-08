48a50ea2fd6db4faabad996b816e485f20f4865d
n-trenear <44279370+n-trenear@users.noreply.github.com>
2019-03-08 10:07:47 +1000

    Update voltageReader.py

--- voltageReader.py
+++ voltageReader.py
@@ -9,8 +9,10 @@ def input_callback0(channel):
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=input_callback1) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(10,GPIO.FALLING,callback=input_callback0) # Setup event on pin 10 rising edge
+if temp == 0:
+    GPIO.add_event_detect(10,GPIO.RISING,callback=input_callback1) # Setup event on pin 10 rising edge
+else:
+    GPIO.add_event_detect(10,GPIO.FALLING,callback=input_callback0) # Setup event on pin 10 rising edge
 
 ## Setup the GPIO for PWM on pin12 {GPIO18}
 GPIO.setmode(GPIO.BOARD)
